829995c5bbddc82b22c5362271ac92351a96edd7
mittz333 <mittz333@gmail.com>
2021-02-15 07:20:53 +0900

    itemモデルの作成

---
+++ db/migrate/20210214221907_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItem < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.integer :category_id, null: false
+      t.text :detail
+      t.integer :location_id, null: false
+      t.integer :department_id, null: false
+      t.date :purchase_date
+      t.references :user, foreign_key: true      
+
+      t.timestamps
+    end
+  end
+end
